0e81acac368927d5e93289288ef8ee83863a463a
josemaia <josecbmaia@hotmail.com>
2016-12-07 10:29:06 +0000

    Add an example of a script that creates events

---
+++ Extensibility Scripts/Engine V2 (C#)/CreateLinesFromArticles.cs
@@ -0,0 +1,41 @@
+﻿using MyMis.Scripting.Core.Contracts;
+using System.Collections.Generic;
+
+
+/*
+Developed by: NumbersBelieve
+Function: Obtains a series of articles from an attribute in the header, and creates a line for each of them.
+Parameters: An interaction with a header attribute named InsertItem, of type Other Entity, based off an External Entity, and with cardinality N.
+  The interaction should have an event in the Details, named PurchaseLine.
+*/
+namespace myMIS
+{
+    public class Script
+    {
+        public ScriptResponse Execute(ContextData context, Entity document, Dictionary<string, object> parameters)
+        {
+            /* **************************************** */
+            /* **************************************** */
+            /*          ADD YOUR CODE HERE              */
+
+            document.MarkToApplyChanges();
+
+            string[] Artigos = document.Attributes.InsertItem.Split(',');
+            foreach (string Item in Artigos)
+            {
+                document.Events.PurchaseLine.Add( new PurchaseLine() );
+
+				        document.Events.PurchaseLine.Last().Attributes.ItemCode = Item;
+            }
+
+
+            /* **************************************** */
+            /* **************************************** */
+
+            return new ScriptResponse
+            {
+                Message = ""
+            };
+        }
+    }
+}
