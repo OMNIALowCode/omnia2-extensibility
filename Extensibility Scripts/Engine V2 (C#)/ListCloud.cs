276c7331938dc4da6a1a6dbfa594696ef6c0819a
josemaia <josecbmaia@hotmail.com>
2016-12-14 18:08:27 +0000

    Add examples of entity list scripts

---
+++ Extensibility Scripts/Engine V2 (C#)/ListCloud.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MyMis.Scripting.Core.Contracts;
+using MyMis.Scripting.ConnectorClient;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+Developed by: NumbersBelieve
+Function: Script that is executed as a button on a list of the platform, and gets a file from an external system. Paired with ListConnector.cs
+
+Parameters: List based off a non-external entity, with the Agent:#Company.Code parameter.
+*/
+
+namespace myMIS
+{
+    public class Script
+    {
+        public ScriptResponse Execute(ContextData context, Entity document, Dictionary<string, object> parameters)
+        {
+            var externalSystem = context.ExternalSystems.Where(e => e.Key == (string)document["Agent:#Company.Code"]).FirstOrDefault().Value;
+
+            var fileToExecute = context.Files.Where(f => f.ExecutionType == "Remote").FirstOrDefault();
+            if (fileToExecute == null)
+            {
+                throw new Exception("File not found");
+            }
+
+            ConnectorClient client = new ConnectorClient(context.Api.Endpoint, externalSystem.ConnectorCode);
+            var connectorResponse = client.Execute(context.Tenant, new MyMis.Scripting.Core.Contracts.Script()
+            {
+                Code = fileToExecute.Name,
+                Version = fileToExecute.Version
+            }, context, document, parameters
+            );
+
+            return new ScriptResponse
+            {
+                RedirectUrl = connectorResponse.Result.FirstOrDefault().Actions["URL"]
+            };
+        }
+		
+		//must use a dictionary, so we can access the Company field by Agent:#Company.Code
+        public class Entity : Dictionary<string,object>
+        {
+        }
+    }
+}
---
+++ Extensibility Scripts/Engine V2 (C#)/ListConnector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MyMis.Scripting.Core.Contracts;
+using MyMis.Scripting.PlatformApi;
+using MyMis.Scripting.PlatformApi.Models;
+using MyMis.Scripting.ConnectorClient;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+/*
+Developed by: NumbersBelieve
+Function: Script that is executed on an external system to return a file to a list that requested it. Paired with ListCloud.cs
+
+Parameters: List based off an entity, with ID and Code fields.
+*/
+namespace myMIS
+{
+    public class Script
+    {
+        public ScriptResponse Execute(ContextData context, Entity document, Dictionary<string, object> parameters)
+        {
+            ApiClient client = new ApiClient(context.Api.Endpoint, context.Tenant);
+            client.Authenticate(context.Api.Email, context.Api.Password);
+
+            string contents = $"Connector accessed from company {context.ExternalSystems.Values.FirstOrDefault().Code} in tenant {context.Tenant}";
+            string fileLocation = null;
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(contents)))
+            {
+                fileLocation = client.BinaryData.UploadFile(ms, "test.txt");
+            }
+
+            var actions = new Dictionary<string, object>();
+            actions.Add("URL", fileLocation);
+
+            return new ScriptResponse
+            {
+                Actions = actions
+            };
+        }
+		public class Entity
+		{
+			int ID { get; set; }
+			string Code { get; set; }
+		}
+    }
+}
